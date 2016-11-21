0af97b201a6b123f60cd0685b2aafc4346ecf9d7
alieliea <3399425930@qq.com>
2016-11-21 12:44:25 +0800

    123123

--- doTest/src/com/test/db/DBManager.java
+++ doTest/src/com/test/db/DBManager.java
@@ -2,11 +2,6 @@ package com.test.db;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-
-import com.test.db.impl.TestDaoImpl;
-import com.test.entity.Test;
-import com.test.util.Pages;
 
 public class DBManager {
 
@@ -59,31 +54,5 @@ public class DBManager {
 	public static File getTable(String tableName){
 		File table = new File(DB_Name + "/" + tableName);
 		return table;
-	}
-
-	public static void main(String[] args) {
-		TestDao dao = new TestDaoImpl(Test.class);
-//		Test test = dao.findById(4);
-//		System.out.println(test == null? "" : test.getA());
-////		test = new Test();
-////		dao.insert(test);
-//		List<Test> list = dao.getALL();
-//		if(list != null && list.size() > 0){
-//			for(Test t : list){
-//				System.out.println(t.getA());
-//			}
-//		}
-//		test.setA(15);
-//		dao.update(test);
-//		dao.delete(5);
-		Pages<Test> page = new Pages<>();
-		page.setPage(3);
-		page.setRows(2);
-		List<Test> list = dao.getALLByPage(page).getList();
-		if(list != null && list.size() > 0){
-			for(Test t : list){
-				System.out.println(t.getA());
-			}
-		}
 	}
 }
