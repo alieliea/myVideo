477fe35ec66cbd42831b7636a4eff06e2d94d7bd
alieliea <3399425930@qq.com>
2016-12-19 13:42:39 +0800

    123

--- api_projects/src/com/api_projects/config/BaseConfig.java
+++ api_projects/src/com/api_projects/config/BaseConfig.java
@@ -2,6 +2,8 @@ package com.api_projects.config;
 
 import java.util.Timer;
 
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.wall.WallFilter;
 import com.api_projects.action.AdminAction;
 import com.api_projects.action.ApiAction;
 import com.api_projects.action.ProjectsAction;
@@ -18,12 +20,12 @@ import com.jfinal.config.Routes;
 import com.jfinal.core.JFinal;
 import com.jfinal.kit.PropKit;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
-import com.jfinal.plugin.c3p0.C3p0Plugin;
+import com.jfinal.plugin.druid.DruidPlugin;
 import com.jfinal.render.ViewType;
 
 public class BaseConfig extends JFinalConfig {
 	private Timer timer = new Timer();
-	
+
 	public void configConstant(Constants me) {
 		PropKit.use("config.txt");
 		me.setDevMode(true);
@@ -32,22 +34,27 @@ public class BaseConfig extends JFinalConfig {
 	}
 
 	public void configRoute(Routes me) {
-		me.add("/admin", AdminAction.class,"/");
-		me.add("/api", ApiAction.class,"/");
-		me.add("/projects", ProjectsAction.class,"/projects");
+		me.add("/admin", AdminAction.class, "/");
+		me.add("/api", ApiAction.class, "/");
+		me.add("/projects", ProjectsAction.class, "/projects");
 	}
-	public static C3p0Plugin createC3p0Plugin() {
-		return new C3p0Plugin(PropKit.get("jdbcUrl"), PropKit.get("user"), PropKit.get("password").trim());
+
+	public static DruidPlugin createDruidPlugin() {
+		return new DruidPlugin(PropKit.get("jdbcUrl"), PropKit.get("user"), PropKit.get("password").trim());
 	}
-	
+
 	public void configPlugin(Plugins me) {
-		C3p0Plugin c3p0Plugin = createC3p0Plugin();
-		me.add(c3p0Plugin);
-		ActiveRecordPlugin arp = new ActiveRecordPlugin(c3p0Plugin);
+		DruidPlugin druidPlugin = createDruidPlugin();
+		druidPlugin.addFilter(new StatFilter());
+		WallFilter wall = new WallFilter();
+		wall.setDbType("mysql");
+		druidPlugin.addFilter(wall);
+		me.add(druidPlugin);
+		ActiveRecordPlugin arp = new ActiveRecordPlugin(druidPlugin);
 		arp.setShowSql(PropKit.getBoolean("showSql"));
 		me.add(arp);
 		_MappingKit.mapping(arp);
-		StaticObject.init();//初始化
+		StaticObject.init();// 初始化
 	}
 
 	public void configInterceptor(Interceptors me) {
@@ -55,22 +62,22 @@ public class BaseConfig extends JFinalConfig {
 	}
 
 	public void configHandler(Handlers me) {
-		
+
 	}
 
 	public static void main(String[] args) {
 		JFinal.start("WebRoot", 80, "/", 5);
 	}
-	
+
 	@Override
-    public void afterJFinalStart() {
-        super.afterJFinalStart();
-        timer.schedule(new Task(), 0, 3600000);
-    }
-	
+	public void afterJFinalStart() {
+		super.afterJFinalStart();
+		timer.schedule(new Task(), 0, 3600000);
+	}
+
 	@Override
-    public void beforeJFinalStop() {
-        super.beforeJFinalStop();
-        timer.cancel();
-    }
+	public void beforeJFinalStop() {
+		super.beforeJFinalStop();
+		timer.cancel();
+	}
 }
